07f431ad7eabfd420b70b23cef1d3692f230892a
Duarte Miguel <duarte.miguel@tecnico.ulisboa.pt>
2020-04-29 19:04:48 +0100

    fixed the histogram in the dishes

--- app/src/main/java/pt/ulisboa/tecnico/cmov/foodist/activities/DishActivity.java
+++ app/src/main/java/pt/ulisboa/tecnico/cmov/foodist/activities/DishActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
@@ -122,39 +123,6 @@ public class DishActivity extends AppCompatActivity implements AdapterView.OnIte
 
         authenticateCheck(uploadButton);
 
-        barChart = (BarChart) findViewById(R.id.bargraph);
-
-        ArrayList<BarEntry> barEntries = new ArrayList<>();
-        int numOfZeros = 0;
-        int numOfOnes = 0;
-        int numOfTwos = 0;
-        int numOfThrees = 0;
-        int numOfFours = 0;
-        int numOfFives = 0;
-        for (Float rating : dish.getVoterRatings().values()) {
-            if(rating == 0){ numOfZeros++; }
-            if(rating == 0.5 || rating == 1){ numOfOnes++; }
-            if(rating == 1.5 || rating == 2){ numOfTwos++; }
-            if(rating == 2.5 || rating == 3){ numOfThrees++; }
-            if(rating == 3.5 || rating == 4){ numOfFours++; }
-            if(rating == 4.5 || rating == 5){ numOfFives++; }
-        }
-
-        barEntries.add(new BarEntry(0f, numOfZeros));
-        barEntries.add(new BarEntry(1f, numOfOnes));
-        barEntries.add(new BarEntry(2f, numOfTwos));
-        barEntries.add(new BarEntry(3f, numOfThrees));
-        barEntries.add(new BarEntry(4f, numOfFours));
-        barEntries.add(new BarEntry(5f, numOfFives));
-
-        BarDataSet barDataSet = new BarDataSet(barEntries, "Ratings");
-
-        BarData theData = new BarData(barDataSet);
-        theData.setBarWidth(0.9f);
-        barChart.setData(theData);
-        barChart.setFitBars(true);
-        barChart.invalidate();
-
     }
 
     @Override
@@ -230,6 +198,44 @@ public class DishActivity extends AppCompatActivity implements AdapterView.OnIte
 
         DishImageAdapter dishImageAdapter = new DishImageAdapter(getApplicationContext(), R.layout.list_item_dish_image, this.dish.getImages());
         listOfDishImages.setAdapter(dishImageAdapter);
+
+        barChart = (BarChart) findViewById(R.id.bargraph);
+
+        ArrayList<BarEntry> barEntries = new ArrayList<>();
+        int numOfOnes = 0;
+        int numOfTwos = 0;
+        int numOfThrees = 0;
+        int numOfFours = 0;
+        int numOfFives = 0;
+        for (Float rating : dish.getVoterRatings().values()) {
+            if(rating == 0.5 || rating == 1){ numOfOnes++; }
+            if(rating == 1.5 || rating == 2){ numOfTwos++; }
+            if(rating == 2.5 || rating == 3){ numOfThrees++; }
+            if(rating == 3.5 || rating == 4){ numOfFours++; }
+            if(rating == 4.5 || rating == 5){ numOfFives++; }
+        }
+
+        barEntries.add(new BarEntry(1f, numOfOnes));
+        barEntries.add(new BarEntry(2f, numOfTwos));
+        barEntries.add(new BarEntry(3f, numOfThrees));
+        barEntries.add(new BarEntry(4f, numOfFours));
+        barEntries.add(new BarEntry(5f, numOfFives));
+
+        BarDataSet barDataSet = new BarDataSet(barEntries, "Ratings");
+
+        BarData theData = new BarData(barDataSet);
+        theData.setBarWidth(0.9f);
+        barChart.setData(theData);
+        barChart.setFitBars(false);
+        barChart.getLegend().setEnabled(false);
+        barChart.getAxisRight().setEnabled(false);
+        barChart.getAxisLeft().setEnabled(false);
+        barChart.getXAxis().setEnabled(false);
+        barChart.getDescription().setEnabled(false);
+        barChart.getBarData().setValueTextColor(Color.WHITE);
+        barChart.invalidate();
+
+
 
     }
 
